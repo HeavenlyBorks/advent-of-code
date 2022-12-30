d2842aa70dc5191e8da07206802a4f9be4a99519
borks <steveminecraft899@gmail.com>
2022-12-30 01:10:16 -0600

    day 1 complete

---
+++ 2022/day01.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    with open("input1") as f:
+        elves = []
+        current = []
+        for cal in f:
+            if cal == "\n":
+                elves.append(sum(current))
+                current = []
+            else:
+                current.append(int(cal.strip()))
+        # don't forget the last elf :(
+        elves.append(sum(current))
+
+        for _ in range(3):
+            print(max(elves))
+            elves.remove(max(elves))
