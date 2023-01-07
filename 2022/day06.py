4bcf293b70a329f53636669e9a30a373910ebc93
borks <steveminecraft899@gmail.com>
2023-01-06 19:57:04 -0600

    completed day 6

--- 2022/day06.py
+++ 2022/day06.py
@@ -2,7 +2,19 @@ import sys
 
 
 if __name__ == "__main__":
-    l = open(sys.argv[0]).read().strip()
+    l = open(sys.argv[1]).read().strip()
     for i in range(3, len(l)):
-        for letter in l[i - 4 : i + 1]:
-            pass
+        count = 0
+        for letter in l[i - 3 : i + 1]:
+            count += l[i - 3 : i + 1].count(letter) == 1
+        if count == 4:
+            print(l[i - 3 : i + 1], i + 1)
+            break
+
+    for i in range(13, len(l)):
+        count = 0
+        for letter in l[i - 13 : i + 1]:
+            count += l[i - 13 : i + 1].count(letter) == 1
+        if count == 14:
+            print(l[i - 13 : i + 1], i + 1)
+            break
